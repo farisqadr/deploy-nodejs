09c57d2ecebaa7fab2b1a0a0f70ec5ffcdeabc22
Faris Abdurrahman <faris.code@gmail.com>
2020-10-28 10:56:27 +0700

    practice deploy

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(morgan("dev"));
+
+app.listen(port, function () {
+  console.log("Listening on port : ", port);
+});
+
+app.use("/tes1", (req, res) => {
+  res.json({
+    tes: "test",
+  });
+});
+
+app.use("/tes2", (req, res) => {
+  res.json({
+    tes: "test2",
+  });
+});
+
+app.use("/tes3", (req, res) => {
+  res.json({
+    tes: "test3",
+  });
+});
+
+app.use("/", (req, res) => {
+  res.json({
+    name: "roni ganteng",
+  });
+});
